6486b760b97ab82911a7a452923a17327433a75a
Rohit <162782881+Rohittt18-Er@users.noreply.github.com>
2024-09-05 21:59:19 +0530

    Update api.js

--- client/app/src/auth/api.js
+++ client/app/src/auth/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://e-commerce-1-76i4.onrender.com/api/v1",
 });
 
 api.interceptors.request.use(
